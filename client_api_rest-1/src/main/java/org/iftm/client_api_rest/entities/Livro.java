9230064deea6d90937cca90d7d872a317e171ec8
stf_t <silvanotf@gmail.com>
2024-10-24 21:30:17 -0300

    Commmit Entidade Livros_ Feito por Silvano

---
+++ client_api_rest-1/src/main/java/org/iftm/client_api_rest/entities/Livro.java
@@ -0,0 +1,83 @@
+package org.iftm.client_api_rest.entities;
+
+// feito por Silvano
+
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Livro implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String titulo;
+
+    @Column(nullable = false, length = 100)
+    private String autor;
+
+    @Column(nullable = false, length = 50)
+    private String genero;
+
+    @Column(nullable = false)
+    private int anoPublicacao;
+
+    // Construtores, getters e setters
+    public Livro() {
+    }
+
+    public Livro(Long id, String titulo, String autor, String genero, int anoPublicacao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+}
